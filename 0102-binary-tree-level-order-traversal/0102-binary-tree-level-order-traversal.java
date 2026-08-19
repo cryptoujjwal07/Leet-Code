d616d89bc4d8bd516421aea28836681a947a0c76
Ayushman Gupta <168714448+cryptoujjwal07@users.noreply.github.com>
2026-08-19 10:58:18 +0530

    Time: 2 ms (19.29%), Space: 46.9 MB (22.59%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,31 +15,22 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        HashMap<Integer , List<Integer>> map = new HashMap<>();
+        solve(root , 0 , map);
         List<List<Integer>> ll = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-
-        if(root == null)return ll;
-        queue.offer(root);
-        queue.offer(null);
-        List<Integer> level = new ArrayList<>();
-
-
-        while(!queue.isEmpty()){
-            TreeNode curr = queue.poll();
-            if(curr == null){
-                ll.add(level);
-                level = new ArrayList<>();
-
-                if(!queue.isEmpty()){
-                    queue.offer(null);
-                }
-            }
-            else{
-                level.add(curr.val);
-                if(curr.left != null)queue.offer(curr.left); 
-                if(curr.right != null)queue.offer(curr.right); 
-            }
+        for(int level = 0 ; level < map.size(); level++){
+            ll.add(map.get(level));
         }
+
         return ll;
+
+    }
+    public void solve(TreeNode root , int level , HashMap<Integer , List<Integer>> map){
+        if(root == null)return;
+
+        map.putIfAbsent(level , new ArrayList<Integer>());
+        map.get(level).add(root.val);
+        solve(root.left , level+1 , map);
+        solve(root.right , level+1 , map);
     }
 }
